956e3a65d7b1b3a7d6b6dd39c3da6936d5c06194
LegendaryCammie <onyeamat@gmail.com>
2025-08-21 04:58:39 +0100

    Update file

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/writetech-portfolio/documentation-tooling">
+            to="/docs/writetech-portfolio/documentation-tooling/intro">
             My Portfolio
           </Link>
         </div>
